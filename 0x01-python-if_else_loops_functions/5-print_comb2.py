22d7699723b2d949fb403afacce701158f3ccad2
Timilehin Olusa <olusatimmie07@gmail.com>
2023-01-03 13:28:03 -0800

    Task 5 debbuged

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 for i in range(0, 100):
     if i != 99:
-        print("{}, ".format(i), end="");
+        var = ", "
     else:
-        print("{}".format(i))
+        var = "\n"
+    if i < 10:
+        print("{}".format(0), end="")
+    print("{}".format(i), end=var)
