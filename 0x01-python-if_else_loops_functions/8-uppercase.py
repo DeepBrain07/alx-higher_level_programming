3f78e93becf94aeb08ef2007389a88760b1d3912
Timilehin Olusa <olusatimmie07@gmail.com>
2023-01-03 14:50:38 -0800

    Task 8 debugged

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,5 +3,7 @@ def uppercase(str):
     for w in str:
         if ord(w) > 90:
             var = ord(w) - 32
+        else:
+            var = ord(w)
             print("{}".format(chr(var)), end="")
     print("")
