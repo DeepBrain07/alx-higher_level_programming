ccc94572167691608aed9fc1ffd302f6644e2c02
Timilehin Olusa <olusatimmie07@gmail.com>
2023-01-03 14:17:35 -0800

    Task 6 debugged

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
 for i in range(90):
-    for j in range(10):
-        if j > i:
-           print("{}".format(i), end="")
-           print("{}".format(j), end="")
-           if i != 8 and j != 9:
-               var = ", "
-           else:
-               var = "\n"
-           print(var, end="")
+    for j in range(i + 1, 10):
+        if i == 8 and j == 9:
+            print('89')
+        else:
+            print('{}{}, '.format(i, j), end='')
