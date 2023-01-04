4cc29fdd90b6bfc35ec5a58cdbffa9b417799887
Timilehin Olusa <olusatimmie07@gmail.com>
2023-01-03 16:39:16 -0800

    Task 15 debugged

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 def remove_char_at(str, n):
-    j = 0
-    for i in range(n):
-        strcp[j] = str[i]
-        j += 1
-    for k in range(n, (len(str) + 1)):
-        strcp[k] = str[n + 1]
-    return (strcp)
+    for i in range(n, (len(str) + 1)):
+        str[i] = str[n + 1]
+    str[len(str)] = ""
+    return (str)
