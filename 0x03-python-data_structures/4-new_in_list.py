4a3f0927b5275d85341eb35fb22362ce0a2cf770
Timilehin Olusa <olusatimmie07@gmail.com>
2023-01-07 14:04:12 -0800

    Task 4 debugged

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -3,7 +3,7 @@ def new_in_list(my_list, idx, element):
     new_list = []
     for w in my_list:
         new_list.append(w)
-    if idx < 0 or idx > len(new_list):
+    if idx < 0 or idx >= len(new_list):
         return (new_list)
     new_list[idx] = element
     return (new_list)
