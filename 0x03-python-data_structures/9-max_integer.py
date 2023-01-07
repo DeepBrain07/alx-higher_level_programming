83cebb38f7f86037e852fd764eccd59ae9b84ad8
Timilehin Olusa <olusatimmie07@gmail.com>
2023-01-07 15:39:06 -0800

    Task 9 completed

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if len(my_list) == 0:
+        return (None)
+    list_cp = []
+    for l in my_list:
+        list_cp.append(l)
+    for i in range(len(my_list)):
+        for j in range(len(list_cp)):
+            if my_list[i] < list_cp[j]:
+                break
+            if j == len(list_cp) - 1 and my_list[i] >= list_cp[j]:
+                return (my_list[i])
