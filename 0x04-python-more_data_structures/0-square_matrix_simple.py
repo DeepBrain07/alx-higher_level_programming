031332850dc8966737f29c27ecaebdcc1dc32023
Timilehin Olusa <olusatimmie07@gmail.com>
2023-01-11 09:27:01 -0800

    Task 0 completed

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    new_matrix = []
+    for i in matrix:
+        new_matrix.append(i)
+    for i in range(len(new_matrix)):
+        new_matrix2 = []
+        for j in new_matrix[i]:
+            new_matrix2.append(j*j)
+        new_matrix[i] = new_matrix2
+    return new_matrix
+            
