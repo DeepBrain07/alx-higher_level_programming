f83a91f2559110562961466d3434460ef7f51c2d
Timilehin Olusa <olusatimmie07@gmail.com>
2023-01-23 14:06:45 -0800

    Task 4 debugged

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -13,5 +13,6 @@ def list_division(my_list_1, my_list_2, list_length):
         except IndexError:
             res = 0
             print("out of range")
-        new_list.append(res)
+        finally:
+            new_list.append(res)
     return (new_list)
