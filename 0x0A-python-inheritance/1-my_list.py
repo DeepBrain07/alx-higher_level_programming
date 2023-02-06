e011ba1ec19aa0a8edce73055a16efc44750f3fe
Timilehin Olusa <olusatimmie07@gmail.com>
2023-02-06 13:01:08 -0800

    Task 1 completed

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+    This module inherits from the list class
+"""
+Class MyList(list):
+    """ This class inherits from the list class """
+    def print_sorted(self):
+        """ prints a sorted list """
+        print(sorted(self))
