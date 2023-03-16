dbd0cd1fca786c968d2b7355e17e803e6a5fc475
Timilehin Olusa <olusatimmie07@gmail.com>
2023-03-16 16:41:54 -0700

    Debugged

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,2 +1,2 @@
 --  lists all the cities of California that can be found in the database hbtn_0d_usa
-SELECT hbtn_0d_usa.cities.id, hbtn_0d_usa.cities.name FROM hbtn_0d_usa.cities WHERE hbtn_0d_usa.cities.state_id = hbtn_0d_usa.cities.id ORDER BY hbtn_0d_usa.cities.id ASC;
+SELECT hbtn_0d_usa.cities.id, hbtn_0d_usa.cities.name FROM hbtn_0d_usa.cities WHERE hbtn_0d_usa.cities.id = (SELECT hbtn_0d_usa.states.id WHERE hbtn_0d_usa.states.name = 'California') ORDER BY hbtn_0d_usa.cities.id ASC;
