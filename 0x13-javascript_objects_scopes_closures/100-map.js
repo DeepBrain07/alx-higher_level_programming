cceb8c2e603da0b0209c9d1d4badf906f355dae6
Timilehin Olusa <olusatimmie07@gmail.com>
2023-04-13 15:26:12 -0700

    Debugged

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -4,13 +4,8 @@ const list = require('./100-data').list;
 
 const newList = list.map((x) => {
   const index = list.indexOf(x);
-  if (index === 0) {
-    x *= 0;
-    return (x);
-  } else {
-    x = x * list[index - 1];
-    return (x);
-  }
+  x *= index;
+  return (x);
 });
 console.log(list);
 console.log(newList);
