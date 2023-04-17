f3f792978d3ebe57cfa7d1b634648de62f32b941
Timilehin Olusa <olusatimmie07@gmail.com>
2023-04-17 16:33:31 -0700

    Debugged

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -7,8 +7,8 @@ import sys
 import MySQLdb
 
 if __name__ == "__main__":
-    db = MySql.connect(host="localhost" user=sys.argv[1], port=3306,
-                       passwd=sys.argv[2], db=sys.argv[3])
+    db = MySQLdb.connect(host="localhost" user=sys.argv[1], port=3306,
+                         passwd=sys.argv[2], db=sys.argv[3])
     curr = db.cursor()
     curr.execute("SELECT * FROM states")
     rows = curr.fetchall()
